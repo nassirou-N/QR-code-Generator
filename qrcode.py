5e12059dc28fd1c1c2d18b5a8260b607ae9d0e01
nassirou <nassirou9993@gmail.com>
2023-06-04 14:42:53 +0100

    generator qr image

--- qrcode.py
+++ qrcode.py
@@ -2,11 +2,14 @@ import pyqrcode
 
 
 def qrcode():
-    query = pyqrcode.create(input())
+    query = pyqrcode.create(input("entre qr mot:\n"))
     query.png('qrcode.png',scale=6)
     print('QR code genereted')
 
 
 if __name__ == '__main__':
     qrcode()
-   
+
+
+# install pyqrcode
+# install pypng
